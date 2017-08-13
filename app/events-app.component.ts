9cc7aa54376ad59da49443bc2bc4605b06096b81
ashutosh-jft <ashutosh.ojha@jellyfishtechnologies.com>
2017-08-13 18:14:18 +0530

    Adding POC of Input and Output of nested component

---
+++ app/events-app.component.ts
@@ -0,0 +1,10 @@
+
+import {Component} from '@angular/core'; // This is comming from the systemjs config file
+
+@Component({
+    selector: 'event-app',
+    template: '<event-list> </event-list>'
+})
+export class EventsAppComponent{
+
+}
---
+++ app/events/event-thumbnail.component.ts
@@ -0,0 +1,33 @@
+import {Component, Input, Output, EventEmitter} from '@angular/core';
+
+@Component({
+    selector: 'event-thumbnail', 
+    template: `
+    <div class="well hoverwell thumbnail">
+    <h2>{{event.name}}</h2>
+    <div>Date: {{event.date}}</div>
+    <div>
+      Time: {{event.time}}
+    </div>
+    <div>Price: \${{event.price}}</div>
+    <div>
+      <span>Location: {{event?.location?.address}}</span>
+    </div>
+    <div>
+      Online URL: {{event.onlineUrl}}
+    </div>
+    <button class="btn btn-primary" (click)="handleClickMe()"> Click Me</button>
+  </div>`
+})
+
+export class EventThumbnailComponent {
+    @Input()
+    event: any;
+
+    @Output()
+    eventClick = new EventEmitter()
+    handleClickMe(){
+        console.log('Clicked!!'); 
+        this.eventClick.emit('Foo');
+    }
+}
