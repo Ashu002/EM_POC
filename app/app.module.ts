e67f9850168dd11dc172a94bdca4bfb24d885ead
ashutosh-jft <ashutosh.ojha@jellyfishtechnologies.com>
2017-08-13 19:29:39 +0530

    Adding nav-bar

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {EventsListComponent} from './events/events-list.component';
 import {EventThumbnailComponent} from './events/event-thumbnail.component';
+import {NavbarComponent} from './nav/nav-bar.component';
 import {EventsAppComponent} from './events-app.component';
 
 @NgModule({
@@ -9,7 +10,7 @@ import {EventsAppComponent} from './events-app.component';
         BrowserModule
     ],
     declarations: [
-        EventsAppComponent, EventsListComponent, EventThumbnailComponent
+        EventsAppComponent, EventsListComponent, EventThumbnailComponent, NavbarComponent
     ],
     bootstrap: [
         EventsAppComponent
