544d1ece77a764ef45107834e0a150c23cf2bbcb
ashutosh-jft <ashutosh.ojha@jellyfishtechnologies.com>
2017-08-15 15:11:51 +0530

    Implement Sorting

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -16,6 +16,7 @@ export class EventDetailsComponent{
     event: IEvent
     addMode: boolean = false
     filterBy: string = 'All'
+    sortBy: string = 'votes'
     constructor(private eventService: EventService, private route: ActivatedRoute){
 
     }
